4e83fbb833a3751700ec50aa088fc5235567beb3
TEDIORELEE <tediorelee@iidx.xyz>
2020-02-22 15:32:46 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,16 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-# cd .vuepress/dist
+cd .vuepress/dist
 
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:dwanda/dwanda.github.io.git master
+git push -f https://FebQltwcIW:9335b52e82cece10aa0cd126e2917875de881211@e.coding.net/tediorelee/tediorelee.git master
+
+# https://FebQltwcIW:9335b52e82cece10aa0cd126e2917875de881211@e.coding.net/tediorelee/tediorelee.git
 
 git push
 
-# cd -
+cd -
