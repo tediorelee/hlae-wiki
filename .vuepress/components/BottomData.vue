36ddfe6c792fae6e48fe6587ef195b9ca4626ef4
TEDIORELEE <tediorelee@iidx.xyz>
2020-03-19 19:43:41 +0800

    增加底部公安备案信息

--- .vuepress/components/BottomData.vue
+++ .vuepress/components/BottomData.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="FirstBottomPage">
-    <a href="http://beian.gov.cn/portal/index.do">皖ICP备20002252号</a> | Copyright © 2020
+
+  <div class="footer-container">
+    <div class="FirstBottomPage">
+      <a href="http://beian.gov.cn/portal/index.do">皖ICP备20002252号</a> | Copyright © 2020
+      <div
+        class="police-box"
+        style="width:300px;margin:0 auto; padding:20px 0;"
+      >
+        <a
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=34012302000653"
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"
+        ><img
+            src="../public/beian.png"
+            style="float:left;"
+          />
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">皖公网安备 34012302000653号</p>
+        </a>
+      </div>
+    </div>
+
   </div>
+
 </template>
 
 <script>
@@ -16,5 +36,9 @@ export default {
   padding-top: 30px;
   margin-top: 40px;
   text-align: center;
+}
+
+.police-box {
+  margin: 0 auto;
 }
 </style>
